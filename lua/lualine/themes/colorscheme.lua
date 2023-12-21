f6df52258c632af9f5dc7df00d21813540d5a823
Nikita Ganzha <geekneek28@gmail.com>
2023-12-21 17:55:34 +0200

    change colorscheme

--- lua/lualine/themes/colorscheme.lua
+++ lua/lualine/themes/colorscheme.lua
@@ -18,27 +18,27 @@ local colors = {
 return {
   normal = {
     a = { bg = colors.green, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.dark, fg = colors.green },
+    b = { bg = colors.gray, fg = colors.green },
     c = { bg = colors.gray, fg = colors.white },
   },
   insert = {
     a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
+    b = { bg = colors.lightgray, fg = colors.blue },
     c = { bg = colors.gray, fg = colors.white },
   },
   visual = {
     a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
+    b = { bg = colors.lightgray, fg = colors.purple },
     c = { bg = colors.gray, fg = colors.white },
   },
   replace = {
     a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
+    b = { bg = colors.lightgray, fg = colors.red },
     c = { bg = colors.gray, fg = colors.white },
   },
   command = {
     a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.lightgray, fg = colors.white },
+    b = { bg = colors.lightgray, fg = colors.yellow },
     c = { bg = colors.gray, fg = colors.white },
   },
   inactive = {
