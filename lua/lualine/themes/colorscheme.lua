8daa4e64a254a4ee30b40edfb44142b8f4cd5eb3
Nikita Ganzha <geekneek28@gmail.com>
2023-12-21 18:04:01 +0200

    change colorscheme

--- lua/lualine/themes/colorscheme.lua
+++ lua/lualine/themes/colorscheme.lua
@@ -4,7 +4,7 @@
 -- stylua: ignore
 local colors = {
   gray   = '#3B454C',
-  light_gray = '#849288',
+  light_gray = '#8B998F',
   orange = '#ff9640',
   purple = '#C778DE',
   red    = '#ec5f67',
@@ -25,26 +25,26 @@ return {
   insert = {
     a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.blue },
-    c = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.light_gray },
   },
   visual = {
     a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.purple },
-    c = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.light_gray },
   },
   replace = {
     a = { bg = colors.red, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.red },
-    c = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.light_gray },
   },
   command = {
     a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.yellow },
-    c = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.light_gray },
   },
   inactive = {
     a = { bg = colors.gray, fg = colors.white, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.white },
-    c = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.light_gray },
   },
 }
