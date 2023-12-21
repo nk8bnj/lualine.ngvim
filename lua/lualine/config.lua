ef6a38c847ca3d977015c194632dc89f19d8739d
Nikita Ganzha <geekneek28@gmail.com>
2023-12-21 19:04:35 +0200

    change colorscheme

--- lua/lualine/config.lua
+++ lua/lualine/config.lua
@@ -29,7 +29,7 @@ local config = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'buffers'},
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_x = { 'encoding', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
