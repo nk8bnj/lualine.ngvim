2da3e05059e42b8b492fbb2edce9023d7196e435
Nikita Ganzha <geekneek28@gmail.com>
2024-01-07 20:32:29 +0200

    change color

--- lua/lualine/config.lua
+++ lua/lualine/config.lua
@@ -27,7 +27,7 @@ local config = {
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { '|', 'branch', 'diff', 'diagnostics' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'filetype' },
     lualine_y = { 'progress' },
