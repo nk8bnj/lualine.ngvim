8eb0c758067ce992e385ddbb3a490d674ffdab27
Nikita Ganzha <geekneek28@gmail.com>
2024-01-13 19:39:41 +0200

    change color

--- lua/lualine/themes/test.lua
+++ lua/lualine/themes/test.lua
@@ -3,7 +3,7 @@
 -- Credit itchyny, jackno (lightline)
 -- stylua: ignore
 local colors = {
-  gray       = '#262E33',
+  gray       = '#343F44',
   -- gray       = '#3C464D',
   light_gray = '#8B998F',
   orange     = '#ff9640',
@@ -23,27 +23,27 @@ local colors = {
 
 return {
   normal = {
-    a = { bg = colors.gray, fg = colors.green, gui = 'bold' },
+    a = { bg = colors.green, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.green },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   insert = {
-    a = { bg = colors.gray, fg = colors.blue, gui = 'bold' },
+    a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.blue },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   visual = {
-    a = { bg = colors.gray, fg = colors.purple, gui = 'bold' },
+    a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.purple },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   replace = {
-    a = { bg = colors.gray, fg = colors.red, gui = 'bold' },
+    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.red },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   command = {
-    a = { bg = colors.gray, fg = colors.yellow, gui = 'bold' },
+    a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
     b = { bg = colors.gray, fg = colors.yellow },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
