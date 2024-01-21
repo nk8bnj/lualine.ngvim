ce8f7c5f7087e6c58aafd42906a1e046d6fa631a
Nikita Ganzha <geekneek28@gmail.com>
2024-01-21 14:59:19 +0200

    change configuration

--- lua/lualine/themes/everforest_color.lua
+++ lua/lualine/themes/everforest_color.lua
@@ -21,27 +21,27 @@ local colors = {
 return {
   normal = {
     a = { bg = colors.green, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.gray, fg = colors.green },
+    b = { bg = colors.dark_light, fg = colors.green },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   insert = {
     a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.gray, fg = colors.blue },
+    b = { bg = colors.dark_light, fg = colors.blue },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   visual = {
     a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.gray, fg = colors.purple },
+    b = { bg = colors.dark_light, fg = colors.purple },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   replace = {
     a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.gray, fg = colors.red },
+    b = { bg = colors.dark_light, fg = colors.red },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   command = {
     a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.gray, fg = colors.yellow },
+    b = { bg = colors.dark_light, fg = colors.yellow },
     c = { bg = colors.gray, fg = colors.light_gray },
   },
   inactive = {
